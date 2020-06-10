3cb8b8d45c05f38fe9a576452ad74c9610b92ba2
John Oblenda <joandong2@gmail.com>
2020-06-09 21:04:26 -0400

    few working endpoints

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code away!
+//This import is pulling from node_modules now
+const express = require("express");
+const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
+const cors = require("cors");
+const server = express();
+
+// This is installing some middleware to allow Express
+// to parse JSON request bodies. We'll go more into detail about this later.
+server.use(express.json());
+server.use(cors());
+
+server.use(userRouter);
+server.use(postRouter);
+
+server.listen(8080, () => {
+    console.log("server started on port 8080");
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,68 @@
-const express = require('express');
-
+const express = require("express");
+const users = require("./userDb");
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post("/", (req, res) => {
+    // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", (req, res) => {
+    // do your magic!
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+    // do your magic!
+    users.get().then((users) => {
+        res.json(users);
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", validateUserId(), (req, res) => {
+    // do your magic!
+    res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get("/:id/posts", (req, res) => {
+    // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", (req, res) => {
+    // do your magic!
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:id", (req, res) => {
+    // do your magic!
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+    return (req, res, next) => {
+        users
+            .getById(req.params.id)
+            .then((user) => {
+                if (user) {
+                    // attach the user to the request object, so we can access it later
+                    // without having to access the database again
+                    req.user = user;
+
+                    next();
+                } else {
+                    res.status(404).json({
+                        message: "User not found",
+                    });
+                }
+            })
+            .catch(next);
+    };
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+    // do your magic!
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+    // do your magic!
 }
 
 module.exports = router;
